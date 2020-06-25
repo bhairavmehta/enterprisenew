48fb4575e1e33c9b831c4af22f5a72f25d317948
Ivan Coric <coric.ivan5@gmail.com>
2020-06-25 23:37:50 +0200

    Nits

--- thebox/services/build_dist.sh
+++ thebox/services/build_dist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ "$(python --version | grep 'Python 3.' > /dev/null && echo 1 || echo 0)" == "0" ]; then
+if [ "$(python3 --version | grep 'Python 3.' > /dev/null && echo 1 || echo 0)" == "0" ]; then
     echo "Please use a virtual env that has Python 3 and above"
 fi
 
-python setup.py bdist_wheel
-python setup_orch.py bdist_wheel
-python setup_infer.py bdist_wheel
-python setup_notif.py bdist_wheel
+python3 setup.py bdist_wheel
+python3 setup_orch.py bdist_wheel
+python3 setup_infer.py bdist_wheel
+python3 setup_notif.py bdist_wheel
 
